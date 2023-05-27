ae477b6e6aee29655a0d9a476175df0bdf1ef158
Muhammad Usama <itxusama711@gmail.com>
2023-05-28 00:08:23 +0500

    Create CODE.cpp

---
+++ CODE.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+ 
+void main(int argc,char* argv[])
+{
+pid_t pid;
+int fd[2];
+if(pipe(fd)==-1)
+{
+  printf("Error in creating pipe\n");
+  return;
+}
+ 
+int size=argc-1;
+pid=fork();
+ 
+ 
+ 
+if(pid>0)
+{
+   int arr2[size];
+   for(int i=1;i<argc;i++)
+   {
+     arr2[i-1]=atoi(argv[i]);
+   }
+   
+   
+   write(fd[1],arr2,size*sizeof(arr2));
+   close(fd[1]);
+   
+   
+   wait();
+   
+   
+   
+   int arr3[size];
+   read(fd[0],arr3,size*sizeof(arr3));
+   close(fd[0]);
+   
+   printf("Sorted Array   : ");
+   for(int i=0;i<size;i++)
+   {
+     printf("%d ",arr3[i]);
+   }
+   printf("\n");
+  
+  
+}
+else
+{
+ 
+    int arr[size];
+    read(fd[0],arr,size*sizeof(arr));
+    close(fd[0]);
+    
+    printf("Unsorted Array : ");
+    for(int i=0;i<size;i++)
+    {
+      printf("%d ",arr[i]);
+    }
+    printf("\n");
+    
+    
+    
+    for(int i=0;i<size;i++)
+    {
+      for(int j=0;j<size;j++)
+      {
+      
+        if(arr[i]<arr[j])
+        { 
+          int temp=arr[i];
+          arr[i]=arr[j];
+          arr[j]=temp;
+        }
+      }
+    }
+    
+    write(fd[1],arr,size*sizeof(arr));
+    close(fd[1]);
+}
+}
